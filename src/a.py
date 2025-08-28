17337aa4a65b2cebeebe3d26054c5f56164c31ed
Daniel Jones <daniel.jones@deepl.com>
2025-08-28 21:41:37 +0200

    Updates

---
+++ src/a.py
@@ -0,0 +1,10 @@
+# Some long text
+def func_a():
+    pass
+
+# Some more long text
+def func_b():
+    pass
+
+def current_version():
+    return "1.0.1"
---
+++ src/b.py
@@ -0,0 +1,15 @@
+# Some long text
+def func_a():
+    pass
+
+# Some more long text
+def func_b():
+    pass
+
+def current_version_a():
+    return "1.0.1"
+
+
+def current_version_with_extra():
+    return "1.0.1" + "-extra"
+
